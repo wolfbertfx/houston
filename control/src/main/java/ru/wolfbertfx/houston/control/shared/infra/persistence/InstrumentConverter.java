410ac1eb186879a63bd7eb5dc1833fb25a255b9a
wolfbertfx <wolfbertfx@gmail.com>
2026-09-07 00:56:51 +0800

    refactor: moved to shared (single source)

--- control/src/main/java/ru/wolfbertfx/houston/control/shared/infra/persistence/InstrumentConverter.java
+++ control/src/main/java/ru/wolfbertfx/houston/control/shared/infra/persistence/InstrumentConverter.java
@@ -1,19 +1,19 @@
-package ru.wolfbertfx.houston.control.asset.infa.persistence;
+package ru.wolfbertfx.houston.control.shared.infra.persistence;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import ru.wolfbertfx.houston.common.asset.Instrument;
 
 @Converter(autoApply = true)
-class InstrumentConverter implements AttributeConverter<Instrument, Integer> {
+public class InstrumentConverter implements AttributeConverter<Instrument, Integer> {
 
     @Override
     public Integer convertToDatabaseColumn(Instrument instrument) {
-        return (instrument != null) ? instrument.getId() : null;
+        return instrument == null ? null : instrument.getId();
     }
 
     @Override
     public Instrument convertToEntityAttribute(Integer id) {
-        return Instrument.fromId(id);
+        return id == null ? null : Instrument.fromId(id);
     }
 }
